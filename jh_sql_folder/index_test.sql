c55fb2b94e26ae538ee313dff6fa1bc7d3b64eeb
jihoon <blue_jihoon@naver.com>
2020-02-21 17:31:02 +0900

    update index

--- jh_sql_folder/index_test.sql
+++ jh_sql_folder/index_test.sql
@@ -145,6 +145,13 @@ select *
 from employees.salaries
 order by rand();
 
+# sal_c의 index 등록 순서 변경(from_date, emp_no)
+create table sal_c_rev
+select *
+from employees.salaries
+order by rand();
+
+
 show create table sal;
 show table status;
 select *
@@ -155,15 +162,21 @@ limit 5;
 ALTER TABLE sal_c
     ADD PRIMARY KEY (emp_no);
 
-analyze table sal, sal_c, sal_se;
+analyze table sal, sal_c, sal_se, sal_c_rev;
 
 show index from sal;
 show index from sal_c;
 show index from sal_se;
+show index from sal_c_rev;
 
 # emp_no & from_date를 pk로 지정
 ALTER TABLE sal_c
     ADD PRIMARY KEY (emp_no, from_date);
+
+# from_date & emp_no를 pk로 지정
+alter table sal_c_rev
+    add primary key (from_date, emp_no);
+
 # emp_no & from_date를 (보조)index로 지정
 alter table sal_se
     add index idx_emp_no (emp_no, from_date);
@@ -172,9 +185,17 @@ show table status;
 
 desc sal;
 select * from sal limit 5;
+select * from sal_c limit 5;
+select * from sal_se limit 5;
+select * from sal_c_rev limit 5;
 
 select * from sal where emp_no = 430609;
 select * from sal_c where emp_no = 430609;
+# 강제로 index를 사용하지 않도록 함
+# select * from sal_c ignore index (PRIMARY) where emp_no = 430609;
+# emp_no * 1 = 결과는 동일하게 출력되지만, where 구문에 (컬럼에 대한)연산이 들어갈 경우 full scan으로 동작
+# -> emp_no = 430609/1은 index로 동작한다.
+# select * from sal_c where emp_no*1 = 430609;
 select * from sal_se where emp_no = 430609;
 
 # SELECT SUM(DATA_FREE)
@@ -185,6 +206,27 @@ select * from sal_se where emp_no = 430609;
 
 show global status like 'Innodb_pages_read';
 # select * from sal where emp_no = 430609;
-# select * from sal_c where emp_no = 430610;
-select * from sal_se where emp_no = 430610;
+# select SQL_NO_CACHE * from sal_c where from_date = '2020-02-21';
+select SQL_NO_CACHE * from sal_c where emp_no = 430610;
+# select * from sal_se where emp_no = 430610;
 show global status like 'Innodb_pages_read';
+
+# INSERT value - Non-Index
+insert into sal value ('430610', '99999', '2020-02-21', '2020-02-21');
+# INSERT value - Clustered Index
+insert into sal_c value ('430610', '99999', '2020-02-21', '2020-02-21');
+# INSERT value - Secondary Index
+insert into sal_se value ('430610', '99999', '2020-02-21', '2020-02-21');
+
+# index 컬럼이 여러개 일 때 cardinality에 따른 emp_no, from_date vs from_date, emp_no의 속도 비교
+select * from sal_c where emp_no = 430610 and from_date = '2020-02-21';
+select * from sal_c where emp_no = 430611 and from_date = '2000-12-03';
+select * from sal_c where emp_no = 430612 and from_date = '2001-10-06';
+select * from sal_c where emp_no = 430613 and from_date = '2002-06-06';
+select * from sal_c where emp_no = 430614 and from_date = '1999-09-08';
+
+select * from sal_c_rev where emp_no = 430610 and from_date = '2020-02-21';
+select * from sal_c_rev where emp_no = 430611 and from_date = '2000-12-03';
+select * from sal_c_rev where emp_no = 430612 and from_date = '2001-10-06';
+select * from sal_c_rev where emp_no = 430613 and from_date = '2002-06-06';
+select * from sal_c_rev where emp_no = 430614 and from_date = '1999-09-08';
